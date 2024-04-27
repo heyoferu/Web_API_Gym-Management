43fb2bf56794cb564270e67f049e807e92905495
heyoferu <heyoferu@users.noreply.github.com>
2024-04-27 02:23:01 -0600

    feat(models): add members
    add members model and its props

---
+++ PUMP.models/Members.cs
@@ -0,0 +1,10 @@
+namespace PUMP.models;
+
+public class Members
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Lastname { get; set; }
+    public int? Age { get; set; }
+    public string? Mail { get; set; }
+}
