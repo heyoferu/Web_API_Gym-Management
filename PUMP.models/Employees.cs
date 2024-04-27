d67399cbfe34293e7347a692fb070be044fd48d4
heyoferu <heyoferu@users.noreply.github.com>
2024-04-27 02:09:44 -0600

    feat(models): add employee
    add employee model and its props

---
+++ PUMP.models/Employees.cs
@@ -0,0 +1,11 @@
+namespace PUMP.models;
+
+public class Employees
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Lastname { get; set; }
+    public int? Age { get; set; }
+    public string? Mail { get; set; }
+    public string? Password { get; set; }
+}
