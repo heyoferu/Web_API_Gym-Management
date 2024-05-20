392b755295d4435b61415d95098474f7668d665e
siliceoluisa <siliceoluisa@users.noreply.github.com>
2024-05-19 22:09:14 -0600

    feat(controllers): add authentication

---
+++ PUMP.api/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using PUMP.core.BL.Interfaces;
+using PUMP.models;
+
+namespace PUMP.api.Controllers;
+
+[Route("v1/auth")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly IAuth _auth;
+
+    public AuthController(IAuth auth)
+    {
+        _auth = auth;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Auth([FromBody] Users users)
+    {
+        var result = await this._auth.Login(users.Username, users.Password);
+
+        if (result == null)
+        {
+            return Unauthorized(result);
+        }
+
+        return Ok(result);
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] Users users)
+    {
+        var result = await this._auth.Register(users.Username, users.Password);
+
+        if (result == null)
+        {
+            return BadRequest(result);
+        }
+
+        return Ok(result);
+    }
+}
+    
+
