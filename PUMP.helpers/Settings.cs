a4ec22949a3590d0a11b42d237e50efc5952e8ab
SaMods12 <SaMods12@users.noreply.github.com>
2024-05-19 22:03:42 -0600

    feat(helpers): define Key and Issuers props to store Jwt strings

--- PUMP.helpers/Settings.cs
+++ PUMP.helpers/Settings.cs
@@ -3,4 +3,6 @@ namespace PUMP.helpers;
 public class Settings
 {
     public static string? ConnectionString { get; set; }
+    public static byte[]? Key { get; set; }
+    public static string? Issuer { get; set; }
 }
